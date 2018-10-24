c4b1639af69a710a01b0d59ddcad17f3c4f5700b
ljh976 <ljh976@tamu.edu>
2018-10-24 13:20:11 -0500

    popup windows from buttons

--- app/src/main/java/com/example/fayyaz/eyedoorbell/MainActivity.java
+++ app/src/main/java/com/example/fayyaz/eyedoorbell/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.fayyaz.eyedoorbell;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.app.Dialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, pop_livefeed.class));
+            }
+
+        });
     }
 }
